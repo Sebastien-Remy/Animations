fe7031b48138867c53d6914ed2ed736eef112588
Sebastien REMY <seb.remy@me.com>
2020-07-05 09:59:56 +0200

    List of users

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,42 +8,22 @@
 
 import SwiftUI
 
-struct CornerRotateModifier: ViewModifier {
-    let amount: Double
-    let anchor: UnitPoint
-    
-    func body(content: Content) -> some View {
-        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
-    }
-}
-
-extension AnyTransition {
-    static var pivot: AnyTransition {
-        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading),
-                  identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
-    }
-}
-
 struct ContentView: View {
     
-    @State private var isShowingRed = false
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        VStack {
-            Button("Tap me") {
-                withAnimation {
-                    self.isShowingRed.toggle()
-                }
-            }
-            if isShowingRed {
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: 200, height: 200)
-                    //.transition(.scale)
-                    //.transition(.asymmetric(insertion: .scale, removal: .opacity))
-                    .transition(.pivot)
+        Button("Tap Me") {
+            // do nothing
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
             }
         }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
